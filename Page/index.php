256003cdc87eb836bc6a8772c8a36056ffb18e40
IgorOtr <igorotero.muniz@gmail.com>
2024-01-23 16:10:51 -0300

    Página de vendas

---
+++ Page/index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <title>Humanalitics - Pages</title>
+
+    <style>
+
+        body {
+            height: 100vh;
+            background-color: #1c1c1c;
+        }
+
+        .container {
+            padding-top: 90px;
+        }
+
+        h1, h2, h3, h4, h5, h6 {
+            font-weight: 600;
+            font-family: Poppins, sans-serif;
+            color: #ffffff;
+            margin: 0px;
+        }
+
+        .btn-success {
+            background-color: #00d100;
+            padding-left: 60px;
+            padding-right: 60px;
+            padding-bottom: 8px;
+            padding-top: 8px;
+        }
+
+        .btn-success:hover {
+            background-color: #007900;
+        }
+
+        .btn-secondary {
+            background-color: #ffffff00;
+            padding-left: 30px;
+            padding-right: 30px;
+            padding-bottom: 8px;
+            padding-top: 8px;
+        }
+
+    </style>
+</head>
+
+<body>
+
+        <div class="container">
+
+            <div class="row mb-5">
+                <div class="col-md-12 text-center mb-3">
+                    <img src="../images/humanalitics/logoo.png" width="300" alt="">
+                </div>
+            </div>
+
+            <div class="row mb-4">
+                <div class="col-md-12 text-center">
+                    <h3>ASSISTA O VÍDEO PARA MAIS INFORMAÇÕES</h3>
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <div class="col-md-3 text-center mb-3">
+                </div>
+                <div class="col-md-6 text-center mb-3">
+                    <div class="card"></div>
+                </div>
+                <div class="col-md-3 text-center mb-3">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <div class="col-md-4 text-center mb-3">
+                </div>
+                <div class="col-md-4 text-center mb-3">
+                    <a href="" target="_blank" class="btn btn-success"><h5>QUERO UM ORÇAMENTO</h5></a>
+                </div>
+                <div class="col-md-4 text-center mb-3">
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-4 text-center mb-3">
+                </div>
+                <div class="col-md-4 text-center mb-3">
+                    <a href="" target="_blank" class="btn btn-secondary"><h5>FALAR COM A EQUIPE</h5></a>
+                </div>
+                <div class="col-md-4 text-center mb-3">
+                </div>
+            </div>
+
+        </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
